a15b4b272387813bd7830365158cf1735977c1b0
Carl Caum <carl@carlcaum.com>
2015-05-22 16:21:34 -0700

    Create Vagrant environment
    This commit introduces a Vagrant environment that can spin up a PE
    master and RH Satellite 6 server to do necessary development on this
    module

---
+++ config/scripts/install_satellite.sh
@@ -0,0 +1,12 @@
+if ! [ -d /opt/satellite ]; then
+  mkdir -p /mnt/iso
+  curl http://osmirror.delivery.puppetlabs.net/iso/satellite-6.0.4-rhel-7-x86_64-dvd.iso > /tmp/satellite-6.0.4-rhel-7-x86_64-dvd.iso
+  mount /tmp/satellite-6.0.4-rhel-7-x86_64-dvd.iso -o loop /mnt/iso
+  
+  cd /mnt/iso
+  /mnt/iso/install_packages
+
+  katello-installer --capsule-dns false --capsule-tftp true --foreman-admin-password "puppetlabs"
+
+  puppet agent -t
+fi
