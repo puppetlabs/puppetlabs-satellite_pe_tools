2e2c94c2ff56845b34664d74bc16bfb460cbe1e4
Carl Caum <carl@carlcaum.com>
2015-05-27 15:59:41 -0700

    Allow multiple hosts to be specified to install PE

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -5,25 +5,36 @@ def project_root
   File.expand_path(File.join(File.dirname(__FILE__), '..'))
 end
 
-def install_pe_on(host)
-  #process the version files if necessary
-  host['pe_dir'] ||= options[:pe_dir]
-  host['pe_ver'] = host['pe_ver'] || options['pe_ver'] ||
-   Beaker::Options::PEVersionScraper.load_pe_version(host[:pe_dir] || options[:pe_dir], options[:pe_version_file])
-
-  #send in the global options hash
-  do_install [host], options
+def find_hosts_with_role(role)
+  hosts.select { |h| h[:roles].include?(role) }
 end
 
-def prepare_satellite_on(target_hosts)
-  Array(target_hosts).each do |host|
+def install_pe_on(role)
+  target_hosts = find_hosts_with_role role
+
+  target_hosts.each do |host|
+    #process the version files if necessary
+    host['pe_dir'] ||= options[:pe_dir]
+    host['pe_ver'] = host['pe_ver'] || options['pe_ver'] ||
+     Beaker::Options::PEVersionScraper.load_pe_version(host[:pe_dir] || options[:pe_dir], options[:pe_version_file])
+
+    pe_installed = (on host, '[ -d /etc/puppetlabs ]').exit_code == 0
+    unless pe_installed
+      #send in the global options hash
+      do_install host, options
+    end
+  end
+end
+
+def prepare_satellite_on(role)
+  target_hosts = find_hosts_with_role role
+  target_hosts.each do |host|
     fqdn = fact_on host, 'fqdn'
     on host, "grep #{fqdn} /etc/hosts || sed -i 's/satellite/#{fqdn} satellite/' /etc/hosts"
   end
 end
 
-install_pe_on master
-on master, "mkdir -p #{master['distmoduledir']}"
+install_pe_on 'master'
 
 prepare_satellite_on 'satellite'
 run_script_on 'satellite', project_root + '/config/scripts/install_satellite.sh'
