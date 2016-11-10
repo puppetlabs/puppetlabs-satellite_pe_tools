1cf6a4ff2a37db83dc6b150d84e61b17cdd84666
Jesse Reynolds <jesse@va.com.au>
2016-11-11 00:05:43 +1100

    MODULES-4083 - add RHEL 6 support (#30)
    * fix rspec test
    * include /bin in path to exec for rhel6 support
    * update README to include support for RHEL 6

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 describe 'satellite_pe_tools' do
 
   context 'with defaults for all parameters' do
-	let(:params) {{ :satellite_url => 'https://127.0.0.1' }}
-	it { should contain_class('satellite_pe_tools') }
+    let(:params) {{ :satellite_url => 'https://127.0.0.1' }}
+    let(:facts) {{ :osfamily => 'RedHat' }}
+    it { should contain_class('satellite_pe_tools') }
   end
 end
