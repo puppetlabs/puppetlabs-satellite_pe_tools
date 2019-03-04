f7160bb71c7a30d91d4efc3888819ec9eaea9614
Eimhin Laverty <eimhin.laverty@puppet.com>
2019-03-04 11:42:36 +0000

    Fix Puppet 5 error in to_yaml_spec.rb

--- spec/functions/to_yaml_spec.rb
+++ spec/functions/to_yaml_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
 describe 'to_yaml' do
+  let(:arg_error) do
+    if Puppet::Util::Package.versioncmp(Puppet.version, '6.0.0') < 0
+      [ArgumentError, 'expects 1 argument']
+    else
+      [Puppet::ParseError, 'Wrong number of arguments']
+    end
+  end
+
   it { is_expected.not_to eq(nil) }
-  it { is_expected.to run.with_params.and_raise_error(Puppet::ParseError, %r{Wrong number of arguments}) }
+  it { is_expected.to run.with_params.and_raise_error(arg_error[0], %r{#{arg_error[1]}}) }
+  it { is_expected.to run.with_params(1, 2).and_raise_error(arg_error[0], %r{#{arg_error[1]}}) }
   it { is_expected.to run.with_params('').and_return("--- ''\n") }
   it {
     is_expected.to run.with_params(['name' => 'bob', 'details' => { 'age' => 247, 'height' => 476 }]).and_return(
