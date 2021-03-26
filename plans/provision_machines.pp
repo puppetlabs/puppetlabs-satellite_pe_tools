f4fc936c8eaf404eb3c419921b40d3aa4f59903f
Daniel Carabas <daniel.carabas@puppet.com>
2021-03-26 11:47:04 +0200

    Fix provision_machines plan

--- plans/provision_machines.pp
+++ plans/provision_machines.pp
@@ -1,5 +1,5 @@
 plan satellite_pe_tools::provision_machines(
 ) {
-  run_task('provision::provision_service', 'localhost', actiont => 'provision', platform => 'centos-7', inventory => './', vars => 'role: ps')
-  run_task('provision::provision_service', 'localhost', actiont => 'provision', platform => 'redhat-7', inventory => './', vars => 'role: satellite')
+  run_task('provision::provision_service', 'localhost', action => 'provision', platform => 'centos-7', inventory => './', vars => 'role: ps')
+  run_task('provision::provision_service', 'localhost', action => 'provision', platform => 'redhat-7', inventory => './', vars => 'role: satellite')
 }
