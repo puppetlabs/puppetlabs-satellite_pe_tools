6f652af52c01c415c28087316341a4308827db00
david22swan <david.swan@puppet.com>
2022-04-12 15:36:38 +0100

    Test

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -25,9 +25,9 @@ RSpec.configure do |c|
     change_target_host(server)
     # Make sure the VM is using our internal DNS servers
     Helper.instance.run_shell("sed -i 's/nameserver.*$/nameserver #{SUT_DNS_SERVER}/' /etc/resolv.conf")
-    sleep(10)
+    sleep(30)
     Helper.instance.run_shell('puppet module install puppetlabs-inifile')
-    sleep(10)
+    sleep(30)
     Helper.instance.run_shell('puppet resource package subscription-manager ensure=installed')
 
     change_target_host(satellite)
