836ee284ef0e052c3b78538b350e23208a18ffe5
Ramesh Sencha <ramesh.sencha@perforce.com>
2023-05-31 21:51:54 +0530

    (CONT-797) - Puppet8 support

--- plans/test_02_server_setup.pp
+++ plans/test_02_server_setup.pp
@@ -3,7 +3,7 @@ plan satellite_pe_tools::test_02_server_setup(
   # get pe-server from inventory file? eg https://puppet.com/docs/bolt/latest/writing_plans.html#collect-facts-from-the-targets
   $server = get_targets('*').filter |$n| { $n.vars['role'] == 'pe' }
   # install pe server
-  run_task('provision::install_pe', $server)
+  run_plan('peadm::install', $server)
 
   # set the ui password
   run_command('puppet infra console_password --password=litmus', $server)
