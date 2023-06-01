11fdc61b28851f6668330a96b3579c814eb4d51e
Ramesh Sencha <ramesh.sencha@perforce.com>
2023-06-01 20:02:50 +0530

    Updating plan for installing PE

--- plans/test_02_server_setup.pp
+++ plans/test_02_server_setup.pp
@@ -3,8 +3,10 @@ plan satellite_pe_tools::test_02_server_setup(
   # get pe-server from inventory file? eg https://puppet.com/docs/bolt/latest/writing_plans.html#collect-facts-from-the-targets
   $server = get_targets('*').filter |$n| { $n.vars['role'] == 'pe' }
   # install pe server
-  run_plan('peadm::install', $server)
-
-  # set the ui password
-  run_command('puppet infra console_password --password=litmus', $server)
+  $params = {
+    'primary_host' => $server,
+    'console_password' => 'litmus',
+    'version' => '2021.7.4',
+  }
+  run_plan('peadm::install', $params)
 }
