e4e3a2467c74d0242ef9ef3039713c540ad8201f
jordanbreen28 <jordan.breen@perforce.com>
2023-06-12 15:26:29 +0100

    (CONT-843) - Add  parameter to server setup

--- plans/server_setup.pp
+++ plans/server_setup.pp
@@ -1,13 +1,12 @@
 plan satellite_pe_tools::server_setup(
+  String[1] $install_dir
 ) {
   # get pe-server from inventory file? eg https://puppet.com/docs/bolt/latest/writing_plans.html#collect-facts-from-the-targets
   $server = get_targets('*').filter |$n| { $n.vars['role'] == 'pe' }
-
-  $install_dir = 'puppet-enterprise-2019.8.13-rc1-154-gb5f3d9c-el-7-x86_64'
   # download PE
-  run_command('gsutil -q cp gs://artifactory-modules/puppet-enterprise-2019.8.13-rc1-154-gb5f3d9c-el-7-x86_64.tar ./', $server)
+  run_command("gsutil -q cp gs://artifactory-modules/${install_dir}.tar ./", $server)
   # extract PE
-  run_command('tar -xf puppet-enterprise-2019.8.13-rc1-154-gb5f3d9c-el-7-x86_64.tar', $server)
+  run_command("tar -xf ${install_dir}.tar", $server)
   # run Installer
   run_command("sudo ${install_dir}/puppet-enterprise-installer -y -c ${install_dir}/conf.d/pe.conf", $server)
 
