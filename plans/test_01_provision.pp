15e5330a3c4d92411efed67e47e256bf3d5721e5
Craig Gumbley <craiggumbley@gmail.com>
2023-06-12 16:06:14 +0100

    (MAINT) Testing ci

--- plans/test_01_provision.pp
+++ plans/test_01_provision.pp
@@ -1,7 +1,7 @@
 plan satellite_pe_tools::test_01_provision(
 ) {
   # provision server machine, set role 
-  run_task('provision::provision_service', 'localhost', action => 'provision', platform => 'centos-stream-8', vars => 'role: pe')
+  run_task('provision::provision_service', 'localhost', action => 'provision', inventory => './', platform => 'centos-stream-8', vars => 'role: pe')
   # provision satellite
-  run_task('provision::provision_service', 'localhost', action => 'provision', platform => 'rhel-8', vars => 'role: satellite')
+  run_task('provision::provision_service', 'localhost', action => 'provision', inventory => './',platform => 'rhel-8', vars => 'role: satellite')
 }
