27f5368734413f20dd5b7a242fddde9fd76a37fa
jordanbreen28 <jordan.breen@perforce.com>
2023-06-13 09:19:16 +0100

    (maint) - amend plan

--- plans/server_setup.pp
+++ plans/server_setup.pp
@@ -10,6 +10,8 @@ plan satellite_pe_tools::server_setup(
   # run Installer
   run_command("sudo ${install_dir}/puppet-enterprise-installer -y -c ${install_dir}/conf.d/pe.conf", $server)
 
+  run_command('echo "autosign = true" >> /etc/puppetlabs/puppet/puppet.conf', $server)
+
   # set the ui password
   run_command('sudo puppet infrastructure console_password --password=litmus', $server)
 }
