081e16f1c49b24c284d98b751c580caea02b3140
Ramesh Sencha <ramesh.sencha@perforce.com>
2023-06-23 14:18:25 +0530

    (CONT-1124) - Adding rspec for manifest

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,16 +1,28 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+
 describe 'satellite_pe_tools' do
-  context 'with defaults for all parameters' do
-    let(:pre_condition) { "service { 'pe-puppetserver': }" }
-    let(:params) { { satellite_url: 'https://127.0.0.1' } }
+  let(:pre_condition) { "service { 'pe-puppetserver': }" }
+  let(:params) { { satellite_url: 'https://127.0.0.1' } }
 
-    on_supported_os.each do |os, facts|
-      context "On #{os}" do
-        let(:facts) { facts }
+  on_supported_os.each do |os, facts|
+    context "On #{os}" do
+      let(:facts) { facts }
 
+      context 'with defaults for all parameters' do
         it { is_expected.to contain_class('satellite_pe_tools') }
+        it { is_expected.to contain_ini_subsetting('reports_satellite') }
+        it { is_expected.to contain_file('satellite_config_yaml') }
+        it { is_expected.to contain_exec('download_install_katello_cert_rpm') }
+        it { is_expected.to contain_file('/etc/puppetlabs/puppet/ssl/ca/katello-default-ca.crt') }
+      end
+
+      context "with manage_default_ca_cert => false and with os family => 'RedHat'" do
+        let(:params) { { satellite_url: 'https://127.0.0.1', manage_default_ca_cert: false } }
+
+        it { is_expected.not_to contain_exec('download_install_katello_cert_rpm') }
+        it { is_expected.not_to contain_file('/etc/puppetlabs/puppet/ssl/ca/katello-default-ca.crt') }
       end
     end
   end
